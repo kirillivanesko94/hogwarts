6511750f532557f658cdaa07ad0f2ac533c34717
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-04-27 21:44:40 +0300

    homework 3.5

--- src/main/java/ru/hogwarts/school1/repositories/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school1/repositories/AvatarRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school1.model.Avatar;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-    Avatar findAvatarByStudentId(Long studentId);
+    Avatar findByStudentId(Long studentId);
 }
--- src/main/java/ru/hogwarts/school1/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school1/service/AvatarService.java
@@ -2,13 +2,12 @@ package ru.hogwarts.school1.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school1.model.Avatar;
 import ru.hogwarts.school1.model.Student;
 import ru.hogwarts.school1.repositories.AvatarRepository;
-import ru.hogwarts.school1.repositories.StudentRepository;
 
-import javax.transaction.Transactional;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,12 +20,10 @@ public class AvatarService {
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
     private final AvatarRepository avatarRepository;
-    private final StudentRepository studentRepository;
     private final StudentService studentService;
 
-    public AvatarService(AvatarRepository avatarRepository, StudentRepository studentRepository,StudentService studentService) {
+    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
         this.avatarRepository = avatarRepository;
-        this.studentRepository = studentRepository;
         this.studentService = studentService;
     }
 
@@ -39,11 +36,14 @@ public class AvatarService {
                 InputStream is = avatarFile.getInputStream();
                 OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
                 BufferedInputStream bis = new BufferedInputStream(is, 1024);
-                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
         ) {
             bis.transferTo(bos);
         }
         Avatar avatar = findAvatar(studentId);
+        if (avatar == null){
+            avatar = new Avatar();
+        }
         avatar.setStudent(student);
         avatar.setFilePath(filePath.toString());
         avatar.setFileSize(avatarFile.getSize());
@@ -52,8 +52,8 @@ public class AvatarService {
         avatarRepository.save(avatar);
     }
 
-    private Avatar findAvatar(Long studentId) {
-        return avatarRepository.findAvatarByStudentId(studentId);
+    public Avatar findAvatar(Long studentId) {
+        return avatarRepository.findByStudentId(studentId);
     }
 
     private String getExtensions(String fileName) {
